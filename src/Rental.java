917ee82958a03bc3e60d665d5ddebd1737c3c2da
MarieKristin <die.gwyn@googlemail.com>
2016-04-27 21:33:14 +0200

    Renamed thisAmount to resultAmount in method getAmount in Rental

--- src/Rental.java
+++ src/Rental.java
@@ -13,22 +13,22 @@ class Rental {
     }
     
     public double getAmount() {
-        double thisAmount = 0;
+        double resultAmount = 0;
         switch (this.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
+                resultAmount += 2;
                 if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+                    resultAmount += (this.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
+                resultAmount += this.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
+                resultAmount += 1.5;
                 if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
+                    resultAmount += (this.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return resultAmount;
     }
 }
