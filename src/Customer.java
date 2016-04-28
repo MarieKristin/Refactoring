6206caea7d8fe2939485ddd246728ed9eb70f571
MarieKristin <die.gwyn@googlemail.com>
2016-04-28 11:04:31 +0200

    Extracted the calculation of frequentRenterPoints into new method

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,4 @@
 
-import java.lang.*;
 import java.util.*;
 
 class Customer {
@@ -15,21 +14,31 @@ class Customer {
         return name;
     };
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();            
-            frequentRenterPoints += each.getFrequentRenterPoints();
+            Rental each = (Rental) enum_rentals.nextElement();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getAmount()) + "\n";
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(this.getTotalAmount()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
+    }
+    
+    private int getTotalFrequentRenterPoints(){
+    	int points = 0;
+    	Enumeration rentals = this.rentals.elements();
+    	
+    	while (rentals.hasMoreElements()) {
+    		Rental each = (Rental) rentals.nextElement();
+    		points += each.getFrequentRenterPoints();
+    	}
+    	
+    	return points;
     }
     
     private double getTotalAmount(){
