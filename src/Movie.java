856f090a24f437a088ca10a734581a76adf04796
MarieKristin <die.gwyn@googlemail.com>
2016-04-28 13:26:23 +0200

    Self Encapsulate Field setPriceCode

--- src/Movie.java
+++ src/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(priceCode);
     }
     public int getPriceCode() {
         return priceCode;
